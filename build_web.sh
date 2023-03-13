d1e471bb35383f7c105c5afdbcaa7c73eb1aed1f
Dimitri LEURS <dimitri.leurs@devoteam.com>
2023-03-13 10:01:46 +0100

    New web release

--- build_web.sh
+++ build_web.sh
@@ -5,6 +5,7 @@ git pull origin main;
 git remote add github git@github.com:dleurs/astrolab.git;
 git push github main --force;
 
+flutter create .;
 flutter build web --base-href="/astrolab/" --release; # web/index.html   <base href="/astrolab/">
 trash docs;
 cp -R build/web docs;
@@ -15,3 +16,4 @@ git push github main;
 # Wait 2 minuts and visit  https://dleurs.github.io/astrolab/
 
 git remote remove github;
+
